7f175aba7caad0ae5a64021ce038ca563c48679c
Deborah Adeoye <debbieadeoye1@gmail.com>
2022-02-20 01:09:14 -0800

    update compiler

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import Search from "./Search";
 import Overview from "./Overview";
 import Developer from "./Developer";
